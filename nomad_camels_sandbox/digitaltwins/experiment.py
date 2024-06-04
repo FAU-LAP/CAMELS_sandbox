b57d2b5cafcc4e79d1aa7bab7739d7ad4f0b90c3
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2024-06-04 09:32:37 +0200

    some updates for experiment, and toml

--- nomad_camels_sandbox/digitaltwins/experiment.py
+++ nomad_camels_sandbox/digitaltwins/experiment.py
@@ -8,12 +8,13 @@ Created on Mon Mar 11 17:45:38 2024
 import json
 from . import heater, diode, semiconductor_resistor, smu, dmm
 
+all_experiments = ["diode_on_heater", "semiconductor_resistor_on_heater"]
+
 
 class experiment:
-    experiments = ['diode_on_heater', 
-                   'semiconductor_resistor_on_heater']
-    
-    def __init__(self, name: str, experiment: str = 'diode_on_heater'):
+    experiments = all_experiments
+
+    def __init__(self, name: str, experiment: str = "diode_on_heater"):
         self.name = name
         self.experiment = experiment
         self.setup(experiment)
@@ -21,12 +22,12 @@ class experiment:
     def setup(self, experiment: str):
         self.experiment = experiment
         if experiment in self.experiments:
-            func = getattr(self, 'setup_'+experiment)
+            func = getattr(self, "setup_" + experiment)
             func()
             return True
         else:
             return False
-        
+
     def setup_diode_on_heater(self):
         heater1 = heater.heater("heater")
         diode1 = diode.diode("diode")
@@ -35,11 +36,11 @@ class experiment:
             diode1,
             smu.smu("smu_heater", heater1),
             dmm.dmm("dmm_pt1000", heater1),
-            smu.smu("smu_diode", diode1)
+            smu.smu("smu_diode", diode1),
         ]
         self.object_temperature_source = heater1
         self.object_samples = [diode1]
-        
+
     def setup_semiconductor_resistor_on_heater(self):
         heater1 = heater.heater("heater")
         semiconductor_resistor1 = semiconductor_resistor.semiconductor_resistor(
@@ -50,20 +51,20 @@ class experiment:
             semiconductor_resistor1,
             smu.smu("smu_heater", heater1),
             dmm.dmm("dmm_pt1000", heater1),
-            smu.smu("smu_sample", semiconductor_resistor1)
+            smu.smu("smu_sample", semiconductor_resistor1),
         ]
         self.object_temperature_source = heater1
         self.object_samples = [semiconductor_resistor1]
-        
+
     def get_object(self, name: str):
         for obj in self.objects:
             if obj.name == name:
                 return obj
-        
+
     def set_temperature(self, temperature: float):
         for sample in self.object_samples:
             sample.set_temperature(temperature)
-                    
+
     def execute_command(self, command: str, value):
         if command.split(".")[0] == self.name:
             if command.split(".")[1] == "setup":
@@ -76,17 +77,14 @@ class experiment:
             else:
                 return (False, None)
         else:
-            # Update temperatures of all samples 
+            # Update temperatures of all samples
             # before any commands are executed
             if self.object_temperature_source is not None:
-                self.set_temperature(
-                    self.object_temperature_source.get_temperature()
-                )
-                
+                self.set_temperature(self.object_temperature_source.get_temperature())
+
             # Execute commands of experiment parts
             for instrument in self.objects:
                 result = instrument.execute_command(command, value)
                 if result is not None:
                     break
             return result
-    
